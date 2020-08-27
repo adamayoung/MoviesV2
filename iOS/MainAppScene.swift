42f2b9b82070a0e943b926e0c91e1cb8cfc60ccc
Adam Young <me@adam-young.co.uk>
2020-08-27 08:51:43 +0100

    CONFIG: Updated TestFlight workflow

---
+++ iOS/MainAppScene.swift
@@ -0,0 +1,27 @@
+//
+//  MainAppScene.swift
+//  Movies (iOS)
+//
+//  Created by Adam Young on 27/08/2020.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct MainAppScene: Scene {
+
+    @ObservedObject var store: AppStore
+
+    init(store: AppStore) {
+        self.store = store
+        WidgetCenter.shared.reloadAllTimelines()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            AppTabNavigation()
+                .environmentObject(store)
+        }
+    }
+
+}
